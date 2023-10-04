9bc3ac8c158062b42e79af6ab211754cfe2b6893
CallMeCJUnderscore <monsterboy09@gmail.com>
2023-10-04 09:46:13 -0400

    completed ShoppingList app

---
+++ workbook-1/ShoppingListProject/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("My current shopping list is:");
+        System.out.println("Milk");
+        System.out.println("Eggs");
+        System.out.println("Greek Yogurt");
+        System.out.println("Celsius");
+        System.out.println("Cookies & Cream Ice Cream");
+        System.out.println("Multigrain Bread");
+        System.out.println("Instant Ramen");
+        System.out.println("Salami");
+        System.out.println("Mayonnaise");
+        System.out.println("Ketchup");
+
+        return;
+    }
+}
