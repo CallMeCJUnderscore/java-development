05fc71b089af6e158d6e6b6c141bb46c30760fc9
CallMeCJUnderscore <monsterboy09@gmail.com>
2023-10-10 13:20:57 -0400

    one more refactoring of project

--- workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
+++ workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,31 +1,29 @@
 package com.pluralsight;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class FullNameApplication {
     public static boolean isStringOnlyAlphabet(String str)
     {
-
-        return ((str != null) && (!str.equals(""))
-                && (str.matches("^[a-zA-Z]*$")));
+        return str.equals("") || str.matches("^[a-zA-Z]*$");
     }
     public static String buildFullName(String firstName, String middleName, String lastName, String suffix){
 
 
         firstName = firstName.toUpperCase().substring(0,1) + firstName.toLowerCase().substring(1);
-        middleName = middleName.toUpperCase().substring(0,1) + middleName.toLowerCase().substring(1);
         lastName = lastName.toUpperCase().substring(0,1) + lastName.toLowerCase().substring(1);
 
         firstName += " ";
-        suffix = ", " + suffix;
 
-        if (middleName.equalsIgnoreCase("NONE")){
-            middleName = "";
+        if(!suffix.equals("")){
+            suffix = ", " + suffix;
         }
-        if (suffix.equalsIgnoreCase(", NONE")){
-            suffix = "";
+
+        if(!middleName.equals("")){
+            middleName = middleName.toUpperCase().substring(0,1) + middleName.toLowerCase().substring(1);
+            middleName+=(middleName.length()==1) ? ". " : " ";
         }
-        middleName+=(middleName.length()==1) ? ". " : " ";
         String fullName = firstName + middleName + lastName + suffix;
 
         return fullName;
@@ -38,40 +36,44 @@ public class FullNameApplication {
 
         Scanner scanner = new Scanner(System.in);
 
-        while (firstName == ""){
+        while (true){
             System.out.print("Please type your first name: ");
             firstName = scanner.nextLine().trim();
             if(!isStringOnlyAlphabet(firstName)){
                 System.out.println("ERROR: Name can only contain letters.");
-                firstName = "";
+                continue;
             }
+            break;
         }
 
-        while (middleName == "" || !middleName.equalsIgnoreCase("NONE")){
-            System.out.print("Please type your middle name (Type NONE if none): ");
+        while (true){
+            System.out.print("Please type your middle name: ");
             middleName = scanner.nextLine().trim();
             if(!isStringOnlyAlphabet(middleName)){
                 System.out.println("ERROR: Name can only contain letters.");
-                middleName = "";
+                continue;
             }
+            break;
         }
 
-        while (lastName == ""){
+        while (true){
             System.out.print("Please type your last name: ");
             lastName = scanner.nextLine().trim();
             if(!isStringOnlyAlphabet(lastName)){
                 System.out.println("ERROR: Name can only contain letters.");
-                lastName = "";
+                continue;
             }
+            break;
         }
 
-        while (suffix == "" || !suffix.equalsIgnoreCase("NONE")){
-            System.out.print("Please type your suffix, if any (Type NONE if none): ");
+        while (true){
+            System.out.print("Please type your suffix, if any: ");
             suffix = scanner.nextLine().trim();
             if(!isStringOnlyAlphabet(suffix)){
                 System.out.println("ERROR: Name can only contain letters.");
-                suffix = "";
+                continue;
             }
+            break;
         }
 
 
