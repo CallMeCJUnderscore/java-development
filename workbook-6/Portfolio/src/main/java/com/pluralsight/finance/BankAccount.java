6179e9c6f9f5c81257f0bc6316e210fdc320ff9a
CallMeCJUnderscore <monsterboy09@gmail.com>
2023-11-14 09:56:19 -0500

    Completed Module 2 Exercise 1 - Intro to Interfaces

---
+++ workbook-6/Portfolio/src/main/java/com/pluralsight/finance/BankAccount.java
@@ -0,0 +1,58 @@
+package com.pluralsight.finance;
+
+public class BankAccount implements IValuable{
+    /*---------------VARIABLES---------------*/
+
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    /*--------------CONSTRUCTORS-------------*/
+
+    public BankAccount(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    /*------------GETTERS/SETTERS------------*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    /*---------------FUNCTIONS---------------*/
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
+}
