9231977d352147c076ef3d7498ac94ca58a74421
CallMeCJUnderscore <monsterboy09@gmail.com>
2023-12-12 15:29:52 -0500

    Completed Module 3 Exercise 4

--- workbook-8/Mod03/NorthwindTraders/src/main/java/com/pluralsight/Program.java
+++ workbook-8/Mod03/NorthwindTraders/src/main/java/com/pluralsight/Program.java
@@ -23,95 +23,89 @@ public class Program {
             return;
         }
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        boolean repeat = true;
-        while (repeat) {
             System.out.print("""
                     What do you want to do?
                     1) Display all products
                     2) Display all customers
+                    3) Display all categories
                     0) Exit
                     Select an option:\s""");
-            String input = scanner.nextLine();
-            if (!input.matches("[12]")) {
-                repeat = false;
+            String command = scanner.nextLine();
+            if (!command.matches("[123]")) {
+                System.out.println("Goodbye...");
             } else {
-                try {
-                    connection = DriverManager.getConnection(
-                            "jdbc:mysql://localhost:3306/northwind",
-                            username,
-                            password
-                    );
+                try (Connection connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/northwind",
+                        username,
+                        password
+                )){
 
-                    if (input.equals("2")) {
-                        preparedStatement = connection.prepareStatement(
-                                """
-                                        SELECT ContactName, CompanyName, City, Country, Phone
-                                        FROM customers
-                                        ORDER BY Country;
-                                        """
-                        );
-
-                        resultSet = preparedStatement.executeQuery();
-
-                        while (resultSet.next()) {
-                            System.out.println("Contact Name: " + resultSet.getString("ContactName"));
-                            System.out.println("Company Name: " + resultSet.getString("CompanyName"));
-                            System.out.println("City: " + resultSet.getString("City"));
-                            System.out.println("Country: " + resultSet.getString("Country"));
-                            System.out.println("Phone Number: " + resultSet.getString("Phone"));
-                            System.out.println("===================================================");
+                    if (command.equals("2")) {
+                        try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT ContactName, CompanyName, City, Country, Phone FROM customers ORDER BY Country");
+                            ResultSet resultSet = preparedStatement.executeQuery()){
+                            while (resultSet.next()) {
+                                System.out.println("Contact Name: " + resultSet.getString("ContactName"));
+                                System.out.println("Company Name: " + resultSet.getString("CompanyName"));
+                                System.out.println("City: " + resultSet.getString("City"));
+                                System.out.println("Country: " + resultSet.getString("Country"));
+                                System.out.println("Phone Number: " + resultSet.getString("Phone"));
+                                System.out.println("===================================================");
+                            }
                         }
-                    } else if (input.equals("1")) {
-                        preparedStatement = connection.prepareStatement(
-                                """
-                                        SELECT ProductID, ProductName, UnitPrice, UnitsInStock
-                                        FROM products
-                                        ORDER BY ProductID;
-                                        """
-                        );
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
 
-                        resultSet = preparedStatement.executeQuery();
+                    } else if (command.equals("1")) {
+                        try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products ORDER BY ProductID;"); ResultSet resultSet = preparedStatement.executeQuery()){
+                            while (resultSet.next()) {
+                                System.out.println("Product ID: " + resultSet.getString("ProductID"));
+                                System.out.println("Product Name: " + resultSet.getString("ProductName"));
+                                System.out.println("Unit Price: $" + resultSet.getString("UnitPrice"));
+                                System.out.println("Units In Stock: " + resultSet.getString("UnitsInStock"));
+                                System.out.println("===================================================");
+                            }
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else if (command.equals("3")){
+                        try(PreparedStatement preparedStatement = connection.prepareStatement(" SELECT CategoryID, CategoryName FROM categories ORDER BY CategoryID");
+                            ResultSet resultSet = preparedStatement.executeQuery()){
+                            while (resultSet.next()) {
+                                System.out.println(resultSet.getString(1) + ") "+ resultSet.getString(2));
+                            }
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("===================================================");
+                        System.out.print("Select a category to search: ");
+                        int toSearch = scanner.nextInt();
+                        try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products WHERE CategoryID LIKE ? ORDER BY ProductID;")){
+                            preparedStatement.setInt(1, toSearch);
 
-                        while (resultSet.next()) {
-                            System.out.println("Product ID: " + resultSet.getString("ProductID"));
-                            System.out.println("Product Name: " + resultSet.getString("ProductName"));
-                            System.out.println("Unit Price: $" + resultSet.getString("UnitPrice"));
-                            System.out.println("Units In Stock: " + resultSet.getString("UnitsInStock"));
-                            System.out.println("===================================================");
+                            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                                while (resultSet.next()) {
+                                    System.out.println("Product ID: " + resultSet.getString("ProductID"));
+                                    System.out.println("Product Name: " + resultSet.getString("ProductName"));
+                                    System.out.println("Unit Price: $" + resultSet.getString("UnitPrice"));
+                                    System.out.println("Units In Stock: " + resultSet.getString("UnitsInStock"));
+                                    System.out.println("===================================================");
+                                }
+                            }
+                            catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
                         }
                     }
 
 
-                } catch (SQLException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
-        if (resultSet != null) {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (preparedStatement != null) {
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
     }
-}
